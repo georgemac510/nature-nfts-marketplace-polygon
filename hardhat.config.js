580fbf5810b94e7331d810c3cc751afb299f26c0
georgemac510 <georgemac510@gmail>
2021-10-11 18:50:07 -0400

    Updated app

---
+++ hardhat.config.js
@@ -0,0 +1,28 @@
+/* hardhat.config.js */
+require("@nomiclabs/hardhat-waffle")
+const fs = require('fs')
+const privateKey = fs.readFileSync(".secret").toString().trim() || "01234567890123456789"
+
+const infuraId = fs.readFileSync(".infuraid").toString().trim() || "";
+
+module.exports = {
+  defaultNetwork: "hardhat",
+  networks: {
+    hardhat: {
+      chainId: 1337
+    },
+    mumbai: {
+      url: `https://polygon-mainnet.infura.io/v3/${infuraId}`,
+      accounts: [privateKey]
+    }
+  },
+  solidity: {
+    version: "0.8.4",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }
+    }
+  }
+}
