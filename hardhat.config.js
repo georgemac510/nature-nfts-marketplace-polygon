24304d14a891c363e764994133ffa4267a20792a
georgemac510 <georgemac510@gmail>
2021-10-29 18:33:54 -0400

    Adding Rinkeby deployment

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,9 +1,8 @@
 /* hardhat.config.js */
-require("@nomiclabs/hardhat-waffle");
-const fs = require('fs');
-const privateKey = fs.readFileSync(".secret").toString().trim() || "01234567890123456789";
+require("@nomiclabs/hardhat-waffle")
+const fs = require('fs')
+const privateKey = fs.readFileSync(".secret").toString().trim() || "01234567890123456789"
 
-// infuraId is optional if you are using Infura RPC
 const infuraId = fs.readFileSync(".infuraid").toString().trim() || "";
 
 module.exports = {
@@ -13,17 +12,13 @@ module.exports = {
       chainId: 1337
     },
     mumbai: {
-      // Infura
-      // url: `https://polygon-mumbai.infura.io/v3/${infuraId}`
-      url: "https://rpc-mumbai.matic.today",
+      url: `https://polygon-mainnet.infura.io/v3/${infuraId}`,
       accounts: [privateKey]
     },
-    matic: {
-      // Infura
-      // url: `https://polygon-mainnet.infura.io/v3/${infuraId}`,
-      url: "https://rpc-mainnet.maticvigil.com",
+    rinkeby: {
+      url: `https://rinkeby.infura.io/v3/${infuraId}`,
       accounts: [privateKey]
-    }
+    },
   },
   solidity: {
     version: "0.8.4",
@@ -34,4 +29,4 @@ module.exports = {
       }
     }
   }
-};
+}
