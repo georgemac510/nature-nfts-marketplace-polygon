962dd69fd4f58c9e0c74cb57f1dda30a728bebfb
georgemac510 <georgemac510@gmail.com>
2022-10-06 12:50:10 -0400

    Update .env and create-nft.js with Next.js reqs

--- pages/create-nft.js
+++ pages/create-nft.js
@@ -5,8 +5,8 @@ import Web3Modal from 'web3modal'
 import { Buffer } from 'buffer';
 import { create as ipfsHttpClient } from 'ipfs-http-client'
 
-const projectId = process.env.INFURA_IPFS_ID;
-const projectSecret = process.env.INFURA_IPFS_SECRET;
+const projectId = process.env.NEXT_PUBLIC_INFURA_IPFS_ID;
+const projectSecret = process.env.NEXT_PUBLIC_INFURA_IPFS_SECRET;
 
 const auth =
   'Basic ' + Buffer.from(projectId + ':' + projectSecret).toString('base64');
